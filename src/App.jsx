cad612135495a9a6cb22ccf03a36e8b85299164b
Hobermac <54035686+Anton1636@users.noreply.github.com>
2024-07-19 13:25:38 +0300

    Add Pricing, roadmap, and footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import ButtonGradient from './assets/svg/ButtonGradient'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Benefits from './components/Benefits'
+import Collaboration from './components/Collaboration'
+import Services from './components/Services'
+import Pricing from './components/Pricing'
+import Roadmap from './components/Roadmap'
+import Footer from './components/Footer'
 
 function App() {
 	return (
@@ -10,6 +15,11 @@ function App() {
 				<Header />
 				<Hero />
 				<Benefits />
+				<Collaboration />
+				<Services />
+				<Pricing />
+				<Roadmap />
+				<Footer />
 			</div>
 			<ButtonGradient />
 		</>
